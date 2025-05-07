61cc6a481ad3568fdeee86ff13c5e3240aafca47
Atik2788 <ashanto653.2@gmail.com>
2025-05-07 22:06:59 +0600

    task 1 completed

--- assi1.ts
+++ assi1.ts
@@ -1,8 +1,36 @@
 {
+    // task 1 start ***************
+    const formatString = (input: string, booleanValue?:boolean) =>{   
+        if(booleanValue === false){
+            return input.toLocaleLowerCase()
+        }
+        else if(booleanValue){
+            return input.toUpperCase()
+        }
+        else{
+            return input.toUpperCase()
+        }
+    }   
 
-    
+    console.log(formatString("Hello"));
+    console.log(formatString("Jello", true));
+    console.log(formatString("Mairallo", false));
+    // task 1 end ***************
+
+
+
+    // task 2 start ***************
+
+
 
 
 
 
 }
+
+
+/* if(typeof input === 'string'){
+    if(booleanValue){
+     return input.toUpperCase()
+    }           
+ } */
