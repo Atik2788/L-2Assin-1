a7befee078ff9d4182adc449ab45d5701f5a121e
Atik2788 <ashanto653.2@gmail.com>
2025-05-08 13:14:43 +0600

    task 7+8 completed

--- assi1.ts
+++ assi1.ts
@@ -197,8 +197,8 @@
         })
     }
 
-    squareAsync(4).then(console.log);        // Output after 1s: 16
-    squareAsync(-3).catch(console.error);    // Output: Error: Negative number not allowed
+    // squareAsync(4).then(console.log);        // Output after 1s: 16
+    // squareAsync(-3).catch(console.error);    // Output: Error: Negative number not allowed
 
     // task 8 End ***************
 
